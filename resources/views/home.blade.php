50d505e60dad9f30273bc301da20d819b93260c3
DANIEL ASSIS GOMES <daniel.agomes7@senacsp.edu.br>
2022-12-01 17:18:06 -0300

    estagio menu site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,9 @@
     <title>Teste</title>
 </head>
 <body>
+  
+<!------------------------------------------------------------- NAVBAR ------------------------------------------------------------------------------->
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
@@ -52,6 +55,86 @@
     </div>
   </div>
 </nav>
+
+<!----------------------------------------------------------- CARROUSEL -------------------------------------------------------------------------------->
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="img/celular1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Para os fotógrafos</h5>
+        <p>O Galaxy S22 Ultra é o melhor celular de 4 câmeras vendido no Brasil</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/celular2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Para você que busca velocidade</h5>
+        <p>Celular com 6GB de RAM: melhores modelos para 2022</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/celular3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Celulares mais em conta</h5>
+        <p>Celulares baratos para comprar por menos de R$ 500</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!----------------------------------------------------------------- Menu ----------------------------------------------------------------------------->
+
+<div class="text-center">
+  <h1>Alguns dos nossos produtos</h1>
+</div>
+<div class="card-group">
+  <div class="card">
+    <img src="/img/jbl.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</html>
+</html> 
